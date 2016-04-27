39325b73147eca8b4129e9c0e2af2a460b3b3a18
Kevin García <gar13177@uvg.edu.gt>
2016-04-27 02:52:59 -0600

    Cambio de color
    cambio de color seteado para el toolbar, tambien camibio de texto

--- app/src/main/java/com/example/kevin/hci/CarElementActivity.java
+++ app/src/main/java/com/example/kevin/hci/CarElementActivity.java
@@ -3,15 +3,21 @@ package com.example.kevin.hci;
 import android.app.Activity;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 public class CarElementActivity extends AppCompatActivity {
+
+    int color = 2;
 
     CarElementAdapter cea;
     ListView listView;
@@ -24,6 +30,10 @@ public class CarElementActivity extends AppCompatActivity {
         setContentView(R.layout.activity_car_element);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Prueba");
+
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.getBackgroundTintList();
@@ -32,6 +42,9 @@ public class CarElementActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                setColor(color);
+                color++;
+                if (color>2) color = 0;
             }
         });
 
@@ -39,6 +52,27 @@ public class CarElementActivity extends AppCompatActivity {
         car_element_rows = getResources().getStringArray(R.array.element_1_rows);
         listView = (ListView)findViewById(R.id.list_car_elements);
         listView.setAdapter(new CarElementAdapter(this,car_element_rows));
+
+    }
+
+    public void setColor(int i){
+        CollapsingToolbarLayout ctl = (CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+        AppBarLayout fl = (AppBarLayout)findViewById(R.id.app_bar);
+
+
+        if (i == 0){//rojo
+            ctl.setContentScrimColor(Color.RED);
+            fl.setBackgroundColor(Color.RED);
+
+        }else if (i == 1){//naranja
+            ctl.setContentScrimColor(Color.parseColor("#FFE57219"));
+            fl.setBackgroundColor(Color.parseColor("#FFE57219"));
+
+        }else{
+            ctl.setContentScrimColor(Color.parseColor("#FF1D8DCE"));
+            fl.setBackgroundColor(Color.parseColor("#FF1D8DCE"));
+        }
+
 
     }
 }
